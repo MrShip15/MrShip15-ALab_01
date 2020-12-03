daad6bf2feec6339e44c3be184861defe31a7f2e
MrShip15 <mr_ship2001@mail.ru>
2020-12-03 23:15:16 +0300

    final commit

---
+++ include/parser.hpp
@@ -0,0 +1,23 @@
+// Copyright 2020 ALexei Kushpelev leha.kushpelev@mail.ru
+#ifndef INCLUDE_PARSER_HPP_
+#define INCLUDE_PARSER_HPP_
+
+#include <string>
+#include <vector>
+
+#include "student.hpp"
+using nlohmann::json;
+
+class Parser {
+ public:
+  explicit Parser(const std::string& jsonPath);
+  void Print_columns(std::ostream& out) const;
+  void Print_lines(std::ostream& out) const;
+  friend std::ostream& operator<<(std::ostream& out, const Parser& pars);
+
+ private:
+  std::vector<Student> students;
+  std::vector<size_t> column_width{0, 9, 7, 15};
+};
+
+#endif  // INCLUDE_PARSER_HPP_
